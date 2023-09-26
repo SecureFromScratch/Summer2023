c22365423444697228b5d615c4c3a7809359068f
yariv <yariv.tal@gmail.com>
2023-09-26 18:34:26 +0300

    removed redundant semicolon

--- pCommitter/input/validator.py
+++ pCommitter/input/validator.py
@@ -2,4 +2,4 @@ _MIN_MSG_LENGTH = 3
 
 # TODO: Add code to this method to only allow messages that DO NOT CONTAIN 4 Letter words
 def isValidCommitMsg(a_sentance: str):
-    return len(a_sentance) >= _MIN_MSG_LENGTH;
+    return len(a_sentance) >= _MIN_MSG_LENGTH
