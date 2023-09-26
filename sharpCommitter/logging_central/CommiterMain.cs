b6d79fb4503e3b7fb712d19c77ec37f5c34bb3ed
yariv <yariv.tal@gmail.com>
2023-09-26 19:57:50 +0300

    central logging c# example

--- sharpCommitter/logging_central/CommiterMain.cs
+++ sharpCommitter/logging_central/CommiterMain.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Logging;
 
 namespace SharpCommitter
 {
-    class CommitterMain
+    class CommitterMain : IDisposable
     {
         public static void Main()
         {
@@ -18,14 +18,16 @@ namespace SharpCommitter
         private readonly ILogger m_logger;
         private readonly Committer m_committer;
         private readonly string m_username;
+        private readonly IDisposable m_scopeDispose;
 
         public CommitterMain(ILogger<CommitterMain> a_logger, Committer a_committer, UsernameProvider a_usernameProvider) 
         {
             m_logger = a_logger;
             m_committer = a_committer;
             m_username = a_usernameProvider.GetUsername();
+            m_scopeDispose = a_logger.BeginScope(new[] { new KeyValuePair<string, object>("username", m_username) });
         }
-
+        
         void Execute()
         {
             Console.WriteLine($"Hello {m_username}, please enter your commit message: (leave empty to view history)");
@@ -36,17 +38,21 @@ namespace SharpCommitter
                 {
                     m_committer.CommitWithMsg(msg);
                     Console.WriteLine("Changes commited with message");
-                    m_logger.LogInformation("{msg} entered by {user}", msg, m_username);
+                    m_logger.LogInformation(msg);
                 }
                 else
                 {
                     Console.WriteLine("Please follow the commit message rules... Aborting commit.");
-                    m_logger.LogWarning("{msg} attempted by {user}", msg, m_username);
+                    m_logger.LogWarning(msg);
                     return;
                 }
             }
 
             m_committer.ShowPreviousCommits();
+        }
+
+        public void Dispose() {
+            m_scopeDispose?.Dispose();
         }
     }
 }
